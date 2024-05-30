cba70da9021565191a6cee3415bb141dbbb9263a
allan zizinga <allanzizinga@yahoo.com>
2024-05-30 15:40:57 +0200

    Create the slot machine rows-transpose

--- project.js
+++ project.js
@@ -132,19 +132,52 @@ const spin = () => {
   }
   //13.Select the results randomly into our wheels,each array will represent a column inside our slot machine
   //We need to generate what is inside each one of them
-  const reels = [[],[],[]]; 
+  const reels = []; 
   for (let i = 0; i < COLS; i++){
-
+       reels.push([]);
        const reelSymbols  = [...symbols];
        for (let j = 0; j < ROWS; j++){
          const randomIndex  = Math.floor(Math.random() * reelSymbols.length);
           const selectedSymbol = reelSymbols[randomIndex];
+          //reels[i] indicates the reel we working at and pushing into the interir array the selected symbol 
+          reels[i].push(selectedSymbol);
+          reelSymbols.splice(randomIndex, 1); //We do this so that this that has been slected isnt selected again and the 1 indicates remove 1, randomIndex is the position which we removing that element.
        }
   }
 
-}
-spin();
+return reels;
+};
 
+
+//14. Transpose which will make the data oputpu appear in columns
+
+const transpose = (reels) => {
+   const rows = [];
+
+   for(let i =0; i < ROWS; i++){
+      rows.push([]);
+      for (let j = 0; j < COLS; j++){
+         rows[i].push(reels[j][i])
+      }
+   }
+   return rows;
+}
+
+
+//15. Print rows
+//This loop is going through each of the rows
+const printRows = (rows) => {
+   for ( const row of rows){
+      let rowString = "" ;
+      for (const [i, symbol] of row.entries()){
+         rowString += symbol
+         if (i != row.length -1){
+            rowString += " | "
+         }
+      }
+      console.log(rowString);
+   }
+}
 
 
 
@@ -154,6 +187,9 @@ let balance = deposit();
 //7.
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels)
+printRows(rows);
 
 
 
